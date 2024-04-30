4d73d8138df321da7d7d0ca0608995cdfe23ed6a
JVolkK <josiasfigueroabidaurre@gmail.com>
2024-04-30 00:14:23 -0300

    Comentar imports inactivos

--- src/containers/BuscarCuidador.js
+++ src/containers/BuscarCuidador.js
@@ -5,8 +5,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../styles/buscarCuidadorStyle.css";
-import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
+//import { Link } from "react-router-dom";
+//import Button from "react-bootstrap/Button";
 
 const BuscarCuidador = () => {
     return (
