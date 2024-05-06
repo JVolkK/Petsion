53a5c83dc5ed13f800d056e5c4a837f8a75b9fcd
JVolkK <josiasfigueroabidaurre@gmail.com>
2024-05-06 04:15:32 -0300

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "../src/containers/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/js/bootstrap.bundle"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
+
 reportWebVitals();
