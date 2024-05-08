de11be24a04e1f64cc27a04be0edcc7090c8c538
emiliasaleme@gmail.com <emiliasaleme@gmail.com>
2024-05-07 23:43:17 -0300

    Arreglado boton en responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,44 @@
-import Container from "react-bootstrap/Container";
-import PETSION from "../images/PETSION.png";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import PETSION from '../images/PETSION.png';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <>
-      <Container fluid className="p-0">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary p-0">
-          <div className="container">
-            <Link className="navbar-brand" to="/">
-              <img src={PETSION} alt="logo" width="180" />
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                {/* Alineación a la derecha */}
-                <li className="nav-item">
-                  <Link className="nav-link" to="/buscar-cuidador">
-                    Buscar cuidador
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/servicios-select">
-                    Servicios
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    Iniciar Sesión
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/registration-select">
-                    Registrarse
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </Container>
+      <Navbar bg="body-tertiary" expand="lg" className="p-0">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">
+            <img src={PETSION} alt="logo" width="180" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarSupportedContent" />
+          <Navbar.Collapse id="navbarSupportedContent">
+            <Nav className="ms-auto mb-2 mb-lg-0">
+              {/* Alineación a la derecha */}
+              <Nav.Item>
+                <Nav.Link as={Link} to="/buscar-cuidador">
+                  Buscar cuidador
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/servicios-select">
+                  Servicios
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/login">
+                  Iniciar Sesión
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/registration-select">
+                  Registrarse
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
