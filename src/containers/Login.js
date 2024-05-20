fcb0c9513f580c639655bfc7fd80e0f444e255e2
JVolkK <josiasfigueroabidaurre@gmail.com>
2024-05-19 21:30:05 -0300

    Login 1905

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,72 +1,72 @@
-import NavBar from "../components/NavBar";
-import React from "react";
-/* import { Link } from "react-router-dom"; */
-import "../styles/login.css";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import '../styles/login.css';
+import { useLogin } from '../hooks/useLogin';
 
-const LoginPage = () => {
+const LoginPage = ({ setAuthenticated }) => {
+  const [emailOrUsername, setEmailOrUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [userType, setUserType] = useState('user');
+  const [error, setError] = useState(null);
+
+  const { handleLogin } = useLogin(setAuthenticated, emailOrUsername, password, userType, setError);
+
   return (
-    <>
-      <section className="vh-100">
-        <Container fluid>
-          <Row>
-            <NavBar /> {/* Agrega el NavBar aquí */}
-            <Col sm={6} className="text-black">
-              <div className="px-5 ms-xl-4">
-                <i
-                  className="fas fa-crow fa-2x me-3 pt-5 mt-xl-4"
-                  style={{ color: "#709085" }}
-                ></i>
-                <span className="h1 fw-bold mb-0"></span>
-              </div>
-              <div className="d-flex align-items-center h-custom-0 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                <Form style={{ width: "23rem" }}>
-                  <h3
-                    className="fw-normal mb-3 pb-3"
-                    style={{ letterSpacing: "1px" }}
+    <section className="vh-100">
+      <Container fluid>
+        <Row>
+          <Col sm={6} className="text-black">
+            <div className="px-5 ms-xl-4">
+              <i className="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style={{ color: '#709085' }}></i>
+              <span className="h1 fw-bold mb-0"></span>
+            </div>
+            <div className="d-flex align-items-center h-custom-0 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+              <Form style={{ width: '23rem' }} onSubmit={handleLogin}>
+                <h3 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>Iniciar Sesión</h3>
+                <Form.Group className="mb-4" controlId="formBasicEmailOrUsername">
+                  <Form.Control
+                    type="text"
+                    placeholder="Correo electrónico o Usuario"
+                    size="lg"
+                    value={emailOrUsername}
+                    onChange={(e) => setEmailOrUsername(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-4" controlId="formBasicPassword">
+                  <Form.Control
+                    type="password"
+                    placeholder="Contraseña"
+                    size="lg"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group controlId="formUserType">
+                  <Form.Label>Seleccionar tipo de cuenta:</Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={userType}
+                    onChange={(e) => setUserType(e.target.value)}
                   >
-                    Iniciar Sesión
-                  </h3>
-                  <Form.Group className="mb-4" controlId="formBasicEmail">
-                    <Form.Control
-                      type="email"
-                      placeholder="Correo electrónico"
-                      size="lg"
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-4" controlId="formBasicPassword">
-                    <Form.Control
-                      type="password"
-                      placeholder="Contraseña"
-                      size="lg"
-                    />
-                  </Form.Group>
-                  <div className="pt-1 mb-4">
-                    <Button variant="info" size="lg" block>
-                      Iniciar sesión
-                    </Button>
-                  </div>
-                  <p className="small mb-5 pb-lg-2">
-                    <a className="text-muted" href="#!">
-                      Olvidaste tu contraseña?
-                    </a>
-                  </p>
-                  <p>
-                    No tenes cuenta?{" "}
-                    <a href="#!" className="link-info">
-                      Regístrate
-                    </a>
-                  </p>
-                </Form>
-              </div>
-            </Col>
-            <Col sm={6} className="px-0 d-none d-sm-block">
-              <div className="LoginImagen backgroundImage" />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </>
+                    <option value="user">Usuario</option>
+                    <option value="anfitrion">Anfitrión</option>
+                  </Form.Control>
+                </Form.Group>
+                {error && <p className="text-danger">{error}</p>}
+                <div className="pt-1 mb-4">
+                  <Button variant="info" size="lg" className="btn-block" type="submit">Iniciar sesión</Button>
+                </div>
+                <p className="small mb-5 pb-lg-2"><a className="text-muted" href="#!">Olvidaste tu contraseña?</a></p>
+                <p>No tienes cuenta? <a href="#!" className="link-info">Regístrate</a></p>
+              </Form>
+            </div>
+          </Col>
+          <Col sm={6} className="px-0 d-none d-sm-block">
+            <div className="LoginImagen backgroundImage" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
