6bffc7aabeb2318b5d061e26121e2cbea5eb6532
Knifucrab <maurosbringas@gmail.com>
2024-05-26 12:29:31 -0300

    fix responsive buscar cuidador

--- src/components/FilterAnfitrionForm.js
+++ src/components/FilterAnfitrionForm.js
@@ -3,8 +3,8 @@ import { AppContext } from "../contexts/AppContext";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 import { useForm } from "../hooks/useFormFiltroAnfitrion";
-import Spinner from "react-bootstrap/Spinner";
 import Button from "react-bootstrap/Button";
 import LoadingOverlay from "../components/LoadingOverlay";
 
@@ -34,79 +34,82 @@ const FilterAnfitrionForm = () => {
     <div>
       <LoadingOverlay loading={loading} />
       <Form onSubmit={handleSubmit}>
-        <Row>
-          <Col>
-            <Form.Group controlId="formServicios">
-              <Form.Label>Servicios</Form.Label>
-              <Form.Control
-                as="select"
-                name="servicio"
-                defaultValue={homeFormValue}
-                onChange={handleChange}
-                disabled={loading}
-              >
-                <option value="alojamiento">Alojamiento</option>
-                <option value="cuidado-dia">Cuidado de día</option>
-                <option value="paseo">Paseo</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="formMascotas">
-              <Form.Label>Mascotas</Form.Label>
-              <Form.Control
-                as="select"
-                name="tipoMascotaAdmite"
-                onChange={handleChange}
-                disabled={loading}
-              >
-                <option value="admitePerro">Perro</option>
-                <option value="admiteGato">Gato</option>
-                <option value="admiteAlltypesMascotas">Otros</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-
-          <Col>
-            <Form.Group controlId="formDiaSemana">
-              <Form.Label>¿En que dia de la semana?</Form.Label>
-              <Form.Control
-                as="select"
-                name="disponibilidadSemana"
-                onChange={handleChange}
-                disabled={loading}
-              >
-                <option value="lunes">Lunes</option>
-                <option value="martes">Martes</option>
-                <option value="miercoles">Miercoles</option>
-                <option value="jueves">Jueves</option>
-                <option value="viernes">Viernes</option>
-                <option value="sabado">Sabado</option>
-                <option value="domingo">Domingo</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="formDiaSemana">
-              <Form.Label>Disponibilidad horaria</Form.Label>
-              <Form.Control
-                as="select"
-                name="disponibilidadHoraria"
-                onChange={handleChange}
-                disabled={loading}
-              >
-                <option value="Mañana">Mañana</option>
-                <option value="Tarde">Tarde</option>
-                <option value="Noche">Noche</option>
-                <option value="Fulltime">Fulltime</option>
-                <option value="Variable">Variable</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-          <Col className="d-flex justify-content-center  m-auto">
-            <Button type="submit">Enviar</Button>
-          </Col>
-        </Row>
+        <Container>
+          <Row className="justify-content-center">
+            <Col xl="3" md="6">
+              <Form.Group controlId="formServicios">
+                <Form.Label>Servicios</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="servicio"
+                  defaultValue={homeFormValue}
+                  onChange={handleChange}
+                  disabled={loading}
+                >
+                  <option value="alojamiento">Alojamiento</option>
+                  <option value="cuidado-dia">Cuidado de día</option>
+                  <option value="paseo">Paseo</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col xl="3" md="6">
+              <Form.Group controlId="formMascotas">
+                <Form.Label>Mascotas</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="tipoMascotaAdmite"
+                  onChange={handleChange}
+                  disabled={loading}
+                >
+                  <option value="admitePerro">Perro</option>
+                  <option value="admiteGato">Gato</option>
+                  <option value="admiteAlltypesMascotas">Otros</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col xl="3" md="6">
+              <Form.Group controlId="formDiaSemana">
+                <Form.Label>¿En que dia de la semana?</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="disponibilidadSemana"
+                  onChange={handleChange}
+                  disabled={loading}
+                >
+                  <option value="lunes">Lunes</option>
+                  <option value="martes">Martes</option>
+                  <option value="miercoles">Miercoles</option>
+                  <option value="jueves">Jueves</option>
+                  <option value="viernes">Viernes</option>
+                  <option value="sabado">Sabado</option>
+                  <option value="domingo">Domingo</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col xl="3" md="6">
+              <Form.Group controlId="formDiaSemana">
+                <Form.Label>Disponibilidad horaria</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="disponibilidadHoraria"
+                  onChange={handleChange}
+                  disabled={loading}
+                >
+                  <option value="Mañana">Mañana</option>
+                  <option value="Tarde">Tarde</option>
+                  <option value="Noche">Noche</option>
+                  <option value="Fulltime">Fulltime</option>
+                  <option value="Variable">Variable</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="d-flex justify-content-center  m-auto pt-3">
+              <Button type="submit">Enviar</Button>
+            </Col>
+          </Row>
+        </Container>
       </Form>
     </div>
   );
