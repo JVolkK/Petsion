16bd68e2670f2fa3e92296222a6069a7db2344de
Knifucrab <maurosbringas@gmail.com>
2024-05-28 17:09:20 -0300

    avances perfilAnfitrion

--- src/containers/PerfilAnfitrion.js
+++ src/containers/PerfilAnfitrion.js
@@ -5,9 +5,11 @@ import Footer from "../components/Footer";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { AppContext } from "../contexts/AppContext";
 import axios from "axios";
+import profileIcon from "../images/145857007_307ce493-b254-4b2d-8ba4-d12c080d6651.jpg";
+import PetsIcon from "@mui/icons-material/Pets";
 
 const PerfilAnfitrion = () => {
-  const { usuarioLogeado, setUsuarioLogeado } = useContext(AppContext);
+  const { setUsuarioLogeado } = useContext(AppContext);
   const [datosAnfitrion, setDatosAnfitrion] = useState({});
 
   const logout = useLogout();
@@ -41,16 +43,35 @@ const PerfilAnfitrion = () => {
       <NavBar />
       <Container className="vh-100">
         <Row className="border">
-          <Col className="border">
-            <h1>{usuarioLogeado.rol}</h1>
-            <h1>{usuarioLogeado.id}</h1>
-            <h1>{datosAnfitrion.name}</h1>
+          <Col className="border" md="5">
+            <img src={profileIcon} className="w-25" alt="profile_icon" />
+            <h1>{`${datosAnfitrion.name} ${datosAnfitrion.lastname}`}</h1>
+            <h2>
+              <h1>{datosAnfitrion.codigoPostal}</h1>
+            </h2>
           </Col>
         </Row>
 
         <Row className="border">
           <Col className="border">
-            <h1>Text 2</h1>
+            <Container className="border ">
+              <Row className="justify-content-center align-items-center">
+                <Col className="justify-content-center align-items-center">
+                  <PetsIcon color="primary" sx={{ fontSize: 100 }} />
+                  <h4>Añadir mascota</h4>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col className="border">
+            <Container className="border">
+              <h3>Añadir mascota</h3>
+            </Container>
+          </Col>
+          <Col className="border">
+            <Container className="border">
+              <h3>Añadir mascota</h3>
+            </Container>
           </Col>
         </Row>
         <Button onClick={logout}>Log out</Button>
