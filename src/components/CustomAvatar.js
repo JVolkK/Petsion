0ec3d9e6b6fcf1f91564f39802e4b549437d7083
emiliasaleme@gmail.com <emiliasaleme@gmail.com>
2024-06-05 18:42:25 -0300

    Se agrega el avatar de anfitrion y se modifica el buscar cuidador

---
+++ src/components/CustomAvatar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+
+const CustomAvatar = ({ nombre, apellido, onClick }) => {
+  const initials = `${nombre.charAt(0)}${apellido.charAt(0)}`;
+
+  // Genera un color aleatorio en formato hexadecimal
+  const randomColor = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  // Aplica el color aleatorio al avatar
+  const color = randomColor();
+
+  return (
+    <Avatar
+      onClick={onClick}
+      sx={{
+        bgcolor: color,
+        width: "6rem", 
+        height: "6rem", 
+        fontSize: "3rem", 
+        borderRadius: "50%", 
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {initials}
+    </Avatar>
+  );
+};
+
+export default CustomAvatar;
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import profileIcon from "../images/145857007_307ce493-b254-4b2d-8ba4-d12c080d6651.jpg";
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 import "../styles/profileCard.css";
 import { GiGrass } from "react-icons/gi";
 import { FaHouse } from "react-icons/fa6";
 import { FaBuilding } from "react-icons/fa";
 import { GiCancel } from "react-icons/gi";
+import CustomAvatar from "./CustomAvatar";
 
 const ProfileCard = ({
   nombre,
@@ -16,33 +16,49 @@ const ProfileCard = ({
   conPatio,
 }) => {
   return (
-    <Card onClick={onClick} className="d-flex flex-row w-100">
-      <img src={profileIcon} className="image w-25 h-25" alt="asd" />
-      <Card.Body>
-        <Card.Title>{`${nombre} ${apellido}`}</Card.Title>
-        {conPatio ? (
-          <div className="d-flex align-items-center">
-            <GiGrass color="green" className="align-items-center" size={30} />
-            <Card.Text>Tiene patio</Card.Text>
-          </div>
-        ) : (
-          <div className="d-flex align-items-center">
-            <GiCancel color="#D83939" size={30} />
-            <Card.Text>Sin patio</Card.Text>
-          </div>
-        )}
-        {tipoDeVivienda === "casa" ? (
-          <div className="d-flex align-items-center">
-            <FaHouse size={30} />
-            <Card.Text>Vive en una casa</Card.Text>
-          </div>
-        ) : (
-          <div className="d-flex align-items-center">
-            <FaBuilding size={30} />
-            <Card.Text>Vive en un departamento</Card.Text>
-          </div>
-        )}
-      </Card.Body>
+    <Card
+      onClick={onClick}
+      className="d-flex flex-row w-100 profile-card"
+      onMouseOver={(e) => e.currentTarget.classList.add("profile-card-hover")}
+      onMouseOut={(e) => e.currentTarget.classList.remove("profile-card-hover")}
+    >
+      <Col
+        xs={3}
+        className="d-flex align-items-center justify-content-center"
+      >
+        <CustomAvatar nombre={nombre} apellido={apellido} onClick={onClick} />
+      </Col>
+      <Col xs={9}>
+        <Card.Body>
+          <Card.Title>{`${nombre} ${apellido}`}</Card.Title>
+          {conPatio ? (
+            <div className="d-flex align-items-center">
+              <GiGrass
+                color="green"
+                className="align-items-center"
+                size={30}
+              />
+              <Card.Text>Tiene patio</Card.Text>
+            </div>
+          ) : (
+            <div className="d-flex align-items-center">
+              <GiCancel color="#D83939" size={30} />
+              <Card.Text>Sin patio</Card.Text>
+            </div>
+          )}
+          {tipoDeVivienda === "casa" ? (
+            <div className="d-flex align-items-center">
+              <FaHouse size={30} />
+              <Card.Text>Vive en una casa</Card.Text>
+            </div>
+          ) : (
+            <div className="d-flex align-items-center">
+              <FaBuilding size={30} />
+              <Card.Text>Vive en un departamento</Card.Text>
+            </div>
+          )}
+        </Card.Body>
+      </Col>
     </Card>
   );
 };
